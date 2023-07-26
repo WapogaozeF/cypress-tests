97a1187532b4fefad7f7b3a5c4ef51f599d23927
Verner Ravil <werner.ravil@gmail.com>
2023-07-26 21:20:23 +0500

    Added test for tasks managment

---
+++ basics-module/cypress/e2e/tasks.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('tasks managments', () => {
+    it('should open and close the new task model', () => {
+        cy.visit('http://localhost:5173/');
+        cy.contains('Add Task').click();
+        cy.get('.backdrop').click({ force: true });
+        cy.get('.backdrop').should('not.exist');
+        cy.get('.modal').should('not.exist');
+    });
+});
