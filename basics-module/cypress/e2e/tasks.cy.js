bd3266a0828b0f7a5f49319d478099e8b99aa5cb
Verner Ravil <werner.ravil@gmail.com>
2023-07-26 21:37:39 +0500

    Added test for create a new task

--- basics-module/cypress/e2e/tasks.cy.js
+++ basics-module/cypress/e2e/tasks.cy.js
@@ -13,4 +13,17 @@ describe('tasks managments', () => {
         cy.get('.backdrop').should('not.exist');
         cy.get('.modal').should('not.exist');
     });
+
+    it('should create a new task', () => {
+        cy.visit('http://localhost:5173/');
+        cy.contains('Add Task').click();
+        cy.get('#title').type('New task');
+        cy.get('#summary').type('Some description');
+        cy.get('.modal').contains('Add Task').click();
+        cy.get('.backdrop').should('not.exist');
+        cy.get('.modal').should('not.exist');
+        cy.get('.task').should('have.length', 1);
+        cy.get('.task h2').contains('New task');
+        cy.get('.task p').contains('Some description');
+    });
 });
