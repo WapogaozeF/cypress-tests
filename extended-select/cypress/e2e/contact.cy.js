ad57d4735de2323849d40f8d1e87ee5615a1cb52
Verner Ravil <werner.ravil@gmail.com>
2023-07-27 23:23:05 +0500

    Fix contact test

--- extended-select/cypress/e2e/contact.cy.js
+++ extended-select/cypress/e2e/contact.cy.js
@@ -10,6 +10,7 @@ describe('contact form', () => {
             expect(el.attr('disabled')).to.be.undefined;
             expect(el.text()).to.contains('Send Message');
         }); 
+        cy.screenshot();
         // cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
         // cy.get('[data-cy="contact-btn-submit"]').should('not.be.disabled');
         cy.get('[data-cy="contact-input-email"]').type('test@test.com{enter}');
@@ -21,28 +22,31 @@ describe('contact form', () => {
     it('should validate the form input', () => {
         cy.visit('http://localhost:5173/about');
         cy.get('[data-cy="contact-btn-submit"]').click();
-        cy.get('[data-cy="contact-btn-submit"]').then(el => {
-            expect(el).to.not.have.attr('disabled');
-            expect(el.text()).to.not.equal('Sending...');
-        });
+        cy.get('[data-cy="contact-btn-submit"]')
+            .should('not.have.attr', 'disabled')
+            .and('not.eq', 'Sending...');
+
         cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
 
         cy.get('[data-cy="contact-input-message"]').as('msgInput');
         cy.get('@msgInput').blur();
-        cy.get('@msgInput').parent().then(el => {
-            expect(el.attr('class')).to.contains('invalid')
-        });
+        cy.get('@msgInput')
+            .parent()
+            .should('have.attr', 'class')
+            .and('match', /invalid/);
 
         cy.get('[data-cy="contact-input-name"]').as('nameInput');
         cy.get('@nameInput').focus().blur();
-        cy.get('@nameInput').parent().then(el => {
-            expect(el.attr('class')).to.contains('invalid')
-        });
+        cy.get('@nameInput')
+            .parent()
+            .should('have.attr', 'class')
+            .and('match', /invalid/);
 
         cy.get('[data-cy="contact-input-email"]').as('emailInput');
         cy.get('@emailInput').focus().blur();
-        cy.get('@emailInput').parent().then(el => {
-            expect(el.attr('class')).to.contains('invalid')
-        });
+        cy.get('@emailInput')
+            .parent()
+            .should('have.attr', 'class')
+            .and('match', /invalid/);
     });
 });
