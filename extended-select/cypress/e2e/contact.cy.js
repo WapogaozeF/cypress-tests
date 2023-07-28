230c3a6e63cf3b1551797a0a09aa88618ac15cb0
Verner Ravil <werner.ravil@gmail.com>
2023-07-28 21:02:16 +0500

    Added commands for reducing code

--- extended-select/cypress/e2e/contact.cy.js
+++ extended-select/cypress/e2e/contact.cy.js
@@ -6,24 +6,24 @@ describe('contact form', () => {
     });
 
     it('should submit the form', () => {
-        cy.get('[data-cy="contact-input-message"]').type('Message text');
-        cy.get('[data-cy="contact-input-name"]').type('John Doe');
-        cy.get('[data-cy="contact-btn-submit"]').as('submitBtn'); 
-        cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+        cy.getById('contact-input-message').type('Message text');
+        cy.getById('contact-input-name').type('John Doe');
+        cy.getById('contact-btn-submit').as('submitBtn'); 
+        cy.getById('contact-btn-submit').then((el) => {
             expect(el.attr('disabled')).to.be.undefined;
             expect(el.text()).to.contains('Send Message');
         }); 
         cy.screenshot();
         // cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
         // cy.get('[data-cy="contact-btn-submit"]').should('not.be.disabled');
-        cy.get('[data-cy="contact-input-email"]').type('test@test.com{enter}');
+        cy.getById('contact-input-email').type('test@test.com{enter}');
         // cy.get('@submitBtn').click();
         cy.get('@submitBtn').contains('Sending...');
         cy.get('@submitBtn').should('be.disabled');
     });
 
     it('should validate the form input', () => {
-        cy.get('[data-cy="contact-btn-submit"]').click();
+        cy.submitForm();
         cy.get('[data-cy="contact-btn-submit"]')
             .should('not.have.attr', 'disabled')
             .and('not.eq', 'Sending...');
